dcde261ad78eaba81edc75702c6ca89334c41d1e
isaeken <hello@isaeken.com.tr>
2020-12-18 01:50:51 +0300

    created entry models

--- app/Models/Entry.php
+++ app/Models/Entry.php
@@ -2,10 +2,45 @@
 
 namespace App\Models;
 
+use App\Enums\EntryType;
+use App\Enums\State;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Entry extends Model
 {
     use HasFactory;
+
+    /**
+     * @var string $table
+     */
+    protected $table = 'entries';
+
+    /**
+     * @var string[] $fillable
+     */
+    protected $fillable = [
+        'state',
+        'type',
+        'password',
+        'content',
+        'expires_at',
+    ];
+
+    /**
+     * @var string[] $casts
+     */
+    protected $casts = [
+        'state' => State::class,
+        'type' => EntryType::class,
+    ];
+
+    /**
+     * @return HasMany
+     */
+    public function keys(): HasMany
+    {
+        return $this->hasMany(EntryKey::class, 'entry_id', 'id');
+    }
 }
