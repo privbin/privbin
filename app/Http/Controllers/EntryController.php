cff2ba071ee7a0128100164c836462a0f805e81b
isaeken <hello@isaeken.com.tr>
2020-12-18 19:57:17 +0300

    created entry store function

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -2,20 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\EntryType;
+use App\Enums\State;
+use App\Http\Requests\EntryRequest;
 use App\Models\Entry;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class EntryController extends Controller
 {
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param EntryRequest $request
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(EntryRequest $request): RedirectResponse
     {
-        //
+        $expires = $request->post('expires');
+        $expires = explode('_', $expires);
+        $expires = Carbon::make("+{$expires[1]} {$expires[0]}");
+
+        $entry = Entry::create([
+            'state' => State::Active(),
+            'type' => $request->post('format'),
+            'password' => strlen($request->post('password')) > 0 ? Hash::make($request->post('password')) : null,
+            'content' => $request->post('content'),
+            'expires_at' => $expires,
+        ]);
+
+        return response()->redirectToRoute('web.entry.show', $entry);
     }
 
     /**
