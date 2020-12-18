01dd570b5ce9c795bf978560869531f3bf5e500d
isaeken <hello@isaeken.com.tr>
2020-12-18 22:36:57 +0300

    created footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="my-5 small text-center text-gray-500">
+    Copyright &copy; {{ config('app.name', 'PrivBin') }} {{ date('Y') }}
+    <br>
+    <a href="https://www.isaeken.com.tr" target="_blank" class="small text-gray-500 text-decoration-none">developed by İsa Eken</a>
+</div>
