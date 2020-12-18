231442202c4f20dc8b39b8d889dfe50a0151b8c0
isaeken <hello@isaeken.com.tr>
2020-12-18 22:55:33 +0300

    created en translation

---
+++ resources/lang/en/privbin.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'new' => 'New',
+    'password' => 'Password',
+    'format' => 'Format',
+    'content' => 'Content',
+    'save' => 'Save',
+    'view' => 'View',
+    'token' => 'Token',
+    'delete' => 'Delete',
+
+    'expires' => 'Expires',
+    'expire_after_minute_5' => 'After 5 minutes',
+    'expire_after_minute_15' => 'After 15 minutes',
+    'expire_after_minute_30' => 'After 30 minutes',
+    'expire_after_hour_1' => 'After 1 hours',
+    'expire_after_hour_3' => 'After 3 hours',
+    'expire_after_hour_6' => 'After 6 hours',
+    'expire_after_day_1' => 'After 1 days',
+
+    'plain_text' => 'Plain Text',
+    'markdown' => 'Markdown',
+
+    'entry_created' => 'Entry created!<br>You can access entry using this address: <a href=":show" target="_blank">:show</a><br>And you can delete your entry directly using token: <kbd>:destroy</kbd>',
+    'again_required_password' => 'Password required again.',
+    'wrong_password' => 'Wrong password',
+
+];
