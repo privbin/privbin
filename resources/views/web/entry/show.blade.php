4b2e75436f958145b2dc5b7563fdca1ee071f9c6
Isa Eken <hello@isaeken.com.tr>
2021-02-15 00:20:51 +0300

    bugfix

--- resources/views/web/entry/show.blade.php
+++ resources/views/web/entry/show.blade.php
@@ -19,7 +19,7 @@
             @endforeach
         @endif
         <div>
-            @if (Auth::check() && $entry->user->id == Auth::user()->id)
+            @if (Auth::check() && $entry->user != null && $entry->user->id == Auth::id())
                 <a href="{{ route("web.entry.edit", $entry) }}" class="block w-full text-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-900 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disable:opacity-25 transition ease-in-out duration-150">
                     {{ __("privbin.edit") }}
                 </a>
